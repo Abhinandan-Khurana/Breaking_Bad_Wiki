f44445bc85fcaffd92d8d9a283b06f5ce530bb8d
Abhinandan-Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2021-08-17 02:46:52 +0530

    breaking bad wiki

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <footer className='center'>
+      <br/>
+        <span className="foot">Created By <a href={"https://abhinandan-khurana.github.io/Portfolio_Website/index.html"}>Abhinandan Khurana</a></span>
+    </footer>
+  )
+}
+
+export default Footer
